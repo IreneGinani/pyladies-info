7f742a66f2a4b3f7b68e0f46113ff5492aa171f4
Irene <ireneginani@gmail.com>
2018-12-11 10:14:44 -0300

    terminando

--- main.py
+++ main.py
@@ -20,15 +20,11 @@ import socket
 import sys  
 import json
 
-reload(sys)  
-sys.setdefaultencoding('utf8')
 
 
 from flask import Flask, jsonify, request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
-import sqlalchemy
-from sqlalchemy import create_engine
 
 integrantes = [
     {
@@ -165,5 +161,5 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8081, debug=True)
 # [END gae_python37_app]
